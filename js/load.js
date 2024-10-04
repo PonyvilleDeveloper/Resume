820e9eee6845a02df65a46b7587212f62e6d4fcf
Kuzin Svyatoslav <92047450+PonyvilleDeveloper@users.noreply.github.com>
2024-10-04 08:47:50 +0000

    Начало работы с цветосхемами

--- js/load.js
+++ js/load.js
@@ -1,6 +1,5 @@
-async function load() {
-    var location = document.location.href
-    for(var i = 0; true; i++) {
+async function load_content() {
+    for(let i = 0; true; i++) {
         let response = await fetch(`/Resume/htm/${i}.htm`)
         if(response.status != 200)
             break;
@@ -10,6 +9,27 @@ async function load() {
         const main = document.getElementsByTagName("main")[0]
         main.appendChild(article)
     }
+}
+
+var COLOR_THEMES = {}
+async function load_colorthemes() {
+    for(let i = 0; true; i++) {
+        let response = await fetch(`Resume/json/${i}.json`)
+        if(response.status != 200)
+            break;
+        let theme = await JSON.parse(response.json())
+        COLOR_THEMES[theme.Name.toString()] = theme
+
+        let select = document.getElementById("color-theme-select")
+        let option = document.createElement("option")
+        option.innerText = theme.Name.toString()
+        option.value = i
+        select.appendChild(option)
+    }
+}
+
+async function load() {
+    await load_content()
 }
 
 document.addEventListener("DOMContentLoaded", load)
