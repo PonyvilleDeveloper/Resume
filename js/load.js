7d42e47f168c455657c6ae64472afcb84aa789e0
Kuzin Svyatoslav <giraffe.sleep@yandex.ru>
2024-10-13 17:59:20 +0300

    -

--- js/load.js
+++ js/load.js
@@ -1,5 +1,4 @@
 async function load() {
-    var location = document.location.href
     for(var i = 0; true; i++) {
         let response = await fetch(`/Resume/htm/${i}.htm`)
         if(response.status != 200)
@@ -7,8 +6,11 @@ async function load() {
         let content = await response.text()
         const article = document.createElement("article")
         article.innerHTML = content
+        const hr = document.createElement("hr")
+
         const main = document.getElementsByTagName("main")[0]
         main.appendChild(article)
+        main.appendChild(hr)
     }
 }
 
